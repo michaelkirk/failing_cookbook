514f94468227dc89fe27f4d87058f3a4d9e8b80d
Michael Kirk <michael.john.kirk@gmail.com>
2014-06-14 01:08:23 -0400

    initial commit of a failing cookbook

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'failing_cookbook'
+maintainer       'Michael Kirk'
+maintainer_email 'michael.john.kirk@gmail.com'
+license          'MIT'
+description      'Installs/Configures failing_cookbook'
+long_description 'Installs/Configures failing_cookbook'
+version          '0.1.0'
+
---
+++ test/integration/default/serverspec/failing_cookbook_spec.rb
@@ -0,0 +1,5 @@
+describe 'failing_cookbook' do
+  it 'should fail' do
+    expect(false).to be_true
+  end
+end
